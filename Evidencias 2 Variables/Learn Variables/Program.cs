22179ad880ef46b5a4b590019934e369f0aa344f
Tumald <1127139@est.intec.edu.do>
2025-01-07 23:21:15 -0400

    Add files via upload

---
+++ Evidencias 2 Variables/Learn Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 5;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("type your first name: ");
+            string myfirstName;
+            myfirstName = Console.ReadLine();
+
+            string mylastName;
+            Console.Write("Type your last name: ");
+            mylastName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + myfirstName + " " +mylastName);
+
+            Console.ReadKey();
+        }
+    }
+}
