42f63ecf4992091e5b7091b122bcc63ee597b81a
Tumald <1127139@est.intec.edu.do>
2025-01-09 18:36:12 -0400

    Add files via upload

---
+++ Evidencias 4 For Iteration/Evidencias 4 For Iteration/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                Console.WriteLine($"12 x {i} = {12 * i}");
+                if (i == 7);
+                {
+                    Console.WriteLine("Found Seven!");
+                    break;
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+            Console.ReadLine();
+        }
+    }
+}     
+            
