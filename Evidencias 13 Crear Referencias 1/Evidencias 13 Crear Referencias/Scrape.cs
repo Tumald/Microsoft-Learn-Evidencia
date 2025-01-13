650db01bbe4b0c170203397d278fcd2ba4762936
Tumald <1127139@est.intec.edu.do>
2025-01-12 23:04:41 -0400

    Add files via upload

---
+++ Evidencias 13 Crear Referencias 1/Evidencias 13 Crear Referencias/Scrape.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace Evidencias_13_Crear_Referencias
+{
+    public class Scrape : ScrapeBase
+    {
+        public string ScrapeWebpage (string url)
+        {
+            return GetWebpage(url);
+            //Console.WriteLine(reply);
+            //File.WriteAllText(url, reply);
+        }
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            //WebClient client = new WebClient();
+            //string reply = client.DownloadString(url);
+
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+    }
+}
---
+++ Evidencias 13 Crear Referencias 1/Evidencias 13 Crear Referencias/ScrapeBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Evidencias_13_Crear_Referencias
+{
+    public class ScrapeBase
+    {
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(url);
+
+        }
+    }
+}
